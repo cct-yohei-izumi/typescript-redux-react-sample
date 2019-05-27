da568031d72080e96ab932e75a514cccae1be472
cct-yohei-izumi <yohei.izumi@cct-inc.co.jp>
2019-05-27 09:23:26 +0900

    viewをまとめる

--- src/components/ViewComponent.tsx
+++ src/components/ViewComponent.tsx
@@ -3,6 +3,7 @@ import { ViewType } from '../constants/ActionTypes';
 import { IHeaderProps } from '../interfaces/PropsInterface';
 import UserSettingContainer from '../containers/UserSettingContainer';
 import OrderContainer from '../containers/OrderContainer';
+import HeaderContainer from '../containers/HeaderContainer';
 
 export default class extends React.Component<IHeaderProps> {
   constructor(props: IHeaderProps) {
@@ -11,6 +12,11 @@ export default class extends React.Component<IHeaderProps> {
   public render() {
     const ViewComponent =
       this.props.selectedViewType === ViewType.USER_VIEW ? <UserSettingContainer /> : <OrderContainer />;
-    return <div>{ViewComponent}</div>;
+    return (
+      <div>
+        <HeaderContainer />
+        {ViewComponent}
+      </div>
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import HeaderContainer from './containers/HeaderContainer';
 import ViewContainer from './containers/ViewContainer';
 import rootReducer from './reducers/RootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -11,7 +10,6 @@ const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-    <HeaderContainer />
     <ViewContainer />
   </Provider>,
   document.getElementById('root') as HTMLElement
